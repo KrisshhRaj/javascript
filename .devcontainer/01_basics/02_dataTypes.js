6a038677292108b67196d99db5ccd1770b654f8e
Krish Raj <162550601+KrisshhRaj@users.noreply.github.com>
2024-03-08 18:54:40 +0000

    DataType

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)// we are using node JS ,not browser
+
+console.log(3 
+    + 3) // code readabality should be high ..hence avoid
+
+console.log("Krish Raj")
+
+let name = "Krish Raj"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number  => 2 to power 53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof "Krish") 
+console.log(typeof null) // typeof null is 'object' which is laungauge deficiency
+console.log(typeof undefined) // typeof undefined is 'undifined' which means undefined is itself one of a type
+console.log(typeof age)
